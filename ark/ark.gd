e3751cddc8d142ea468aead713c34edd972e6b97
sychev2003di <sychev2003di@gmail.com>
2024-09-10 02:46:37 +0300

    change base class for `Cell` and make `Ark` drawable

--- ark/ark.gd
+++ ark/ark.gd
@@ -4,11 +4,35 @@ extends Node2D
 
 const ARRAY_SIZE := Vector2i(7, 7)
 const CENTRAL_CELL_POS := Vector2i(3, 3)
+const CELL_START_POS := Vector2i.ZERO
 var cells: Array[Cell] = []
 
 @export var health: int
 @export var speed: int
 @export var fov: float
+
+@export var cell_scen: PackedScene
+
+
+func _init() -> void:
+	var cur_pos := self.CELL_START_POS 
+	# var cell := self.cell_scen.instantiate() as Cell
+
+	for row in self.ARRAY_SIZE.x:
+		for col in self.ARRAY_SIZE.y:
+			var cell := Cell.new(cur_pos)
+			self.cells.append(cell)
+			cur_pos.x += Cell.CELL_SIZE.y
+
+			self.add_child(cell)
+
+		cur_pos.y += Cell.CELL_SIZE.x 
+		cur_pos.x = self.CELL_START_POS.x
+
+
+func _ready() -> void:
+	print('first cell pos: ' ,self.cells.front().position)
+	print('last cell pos: ', self.cells.back().position)
 
 
 func change_cell(pos: Vector2i, new_cell: Cell) -> void:
--- ark/cell.gd
+++ ark/cell.gd
@@ -1,5 +1,5 @@
 class_name Cell
-extends Sprite2D
+extends TextureButton
 
 
 enum MATERIAL_TYPE {
@@ -13,20 +13,28 @@ const CELL_HP_ARRAY := [50, 100]
 
 @export var health: int 
 
-var store_unit = null
+var store_unit: Object = null
 var material_type: MATERIAL_TYPE
 
 var enabled: bool
 
-func _init(type: MATERIAL_TYPE, pos: Vector2i, enable: bool = true) -> void:
+
+func _init(
+	pos: Vector2i = Vector2i.ZERO,
+	type: MATERIAL_TYPE = MATERIAL_TYPE.VOID,
+	enable: bool = true
+) -> void:
 	self.material_type = type
 	self.health = self.CELL_HP_ARRAY[type]
-	self.position = pos
+	self.set_pos(pos)
 	self.enabled = enable
 
 
 func _ready() -> void:
-	self.texture = null
+	pass
+
+
+func _process(_delta: float) -> void:
 	pass
 
 
@@ -34,9 +42,9 @@ func _draw() -> void:
 	const width := 4.0
 
 	var rect := Rect2(Vector2.ZERO, self.CELL_SIZE)
-	rect.size.x -= width
-	rect.size.y -= width
-	rect.position -= rect.size / 2
+	# rect.size.x -= width
+	# rect.size.y -= width
+	rect.position += Vector2(width, width) / 2
 	
 	draw_rect(rect, Color.BLACK, false, width)
 
@@ -47,3 +55,7 @@ func _draw() -> void:
 func attach_unit(unit: Variant) -> void:
 	print('unit is attached')
 	self.store_unit = unit
+
+
+func set_pos(pos: Vector2) -> void:
+	self.position = pos - Vector2(self.CELL_SIZE / 2)
